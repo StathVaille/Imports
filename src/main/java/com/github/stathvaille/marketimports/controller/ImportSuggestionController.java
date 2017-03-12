5f4124a72289020624fbd71a566d14c8bb758bb8
stath <stathvaille@gmail.com>
2017-03-12 21:16:11 +0000

    Added Rigs and use response wrapper

--- src/main/java/com/github/stathvaille/marketimports/controller/ImportSuggestionController.java
+++ src/main/java/com/github/stathvaille/marketimports/controller/ImportSuggestionController.java
@@ -3,6 +3,8 @@ package com.github.stathvaille.marketimports.controller;
 import com.github.stathvaille.marketimports.domain.ImportSuggestion;
 import com.github.stathvaille.marketimports.domain.ResponseWrapper;
 import com.github.stathvaille.marketimports.service.ImportSuggestionService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,13 +13,13 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
-//@RestController("/api/import")
 @Controller
 @ResponseBody
 @RequestMapping("/api/import")
 public class ImportSuggestionController {
 
     private final ImportSuggestionService importSuggestionService;
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     public ImportSuggestionController(ImportSuggestionService importSuggestionService){
         this.importSuggestionService = importSuggestionService;
@@ -26,6 +28,9 @@ public class ImportSuggestionController {
     @GetMapping
     @CrossOrigin
     public ResponseWrapper<List<ImportSuggestion>> getImportSuggestions(){
-        return new ResponseWrapper(importSuggestionService.getImportSuggestions());
+        logger.info("Calculating market import suggestions...");
+        List<ImportSuggestion> importSuggestions = importSuggestionService.getImportSuggestions();
+        logger.info("Found " + importSuggestions.size() + " import suggestions.");
+        return new ResponseWrapper(importSuggestions);
     }
 }
