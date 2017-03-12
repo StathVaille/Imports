31b97434fe794323c95afa9d648833a55698139b
stath <stathvaille@gmail.com>
2017-03-12 15:02:15 +0000

    Fixed bug in market history service where the wrong history was reported for slow selling items

--- src/main/java/com/github/stathvaille/marketimports/service/MarketHistoryService.java
+++ src/main/java/com/github/stathvaille/marketimports/service/MarketHistoryService.java
@@ -7,7 +7,15 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalUnit;
 import java.util.*;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 /**
@@ -18,6 +26,7 @@ public class MarketHistoryService {
 
     private static final String apiTemplate = "https://esi.tech.ccp.is/latest/markets/%s/history/?datasource=tranquility&type_id=%s";
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
+    private final static DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
 
     public Double getAverageNumberOfSalesInPast7Days(Item item, ImportLocation importLocation){
         logger.info("Calculating volume of " + item.getName() + " sold in the last 7 days in " + importLocation.getRegionName());
@@ -26,22 +35,19 @@ public class MarketHistoryService {
         Map[] itemHistoryArray = restTemplate.getForObject(url, HashMap[].class);
         List<Map> itemHistoryList = Arrays.asList(itemHistoryArray);
 
-        List<Map> last7ItemHistory;
-        if(itemHistoryList.size() > 7) {
-            last7ItemHistory = itemHistoryList.subList(itemHistoryArray.length - 8, itemHistoryArray.length - 1);
-        }
-        else {
-            last7ItemHistory = itemHistoryList;
-        }
+
+        LocalDate sevenDaysAgo = LocalDate.now().minusDays(8);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+        List<Map> last7ItemHistory = itemHistoryList.stream().filter(itemHistory ->  {
+            LocalDate historicDate = LocalDate.parse((String)itemHistory.get("date"), formatter);
+            return historicDate.isAfter(sevenDaysAgo);
+        }).collect(Collectors.toList());
 
         List<Integer> last7ItemVolumes = last7ItemHistory.stream().map(itemHistory -> (int) itemHistory.get("volume")).collect(Collectors.toList());
-        OptionalDouble optionalAverage = last7ItemVolumes.stream().mapToInt(a -> a).average();
-        if (optionalAverage.isPresent()){
-            return optionalAverage.getAsDouble();
-        }
-        else{
-            return 0.0;
-        }
+        int totalSoldInLast7Days = last7ItemVolumes.stream().mapToInt(a -> a).sum();
+        double averageSoldInLast7Days = totalSoldInLast7Days / 7.0;
+        return averageSoldInLast7Days;
     }
 
     public Map<Item, Double> getAverageNumberOfSalesInPast7Days(Collection<Item> items, ImportLocation importLocation){
